446c482af90c8da9855853825fe5c86a637d78df
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-08-16 18:45:12 -0700

    Activating the extension on startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('activating bread extension')
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bread" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bread.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from bread!');
-	});
-	
-	context.subscriptions.push(disposable);
+	// Commands also need to be defined in package.json
+	context.subscriptions.unshift(
+		vscode.commands.registerCommand('bread.run', run)
+	)
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+/**
+ * Collect all files with @bread mention
+ * Pack the files along with the diff generation prompts
+ * Call openai api (through langchain)
+ * Parse the diffs and apply them to the files (in place for now)
+ */
+async function run() {
+	console.log('running bread')
+
+	
+}
