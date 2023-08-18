8e432a6ef4797612ca626296ea4a09e061623a6f
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-08-17 20:16:56 -0700

    Added linter setup docs tho .. minimal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: "module"
-    },
-    plugins: ['@typescript-eslint', 'unused-imports'],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        // Add your custom rules here
-        '@typescript-eslint/no-explicit-any': 'off',
-        'unused-imports/no-unused-imports': 'warn',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'unused-imports'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Add your custom rules here
+    '@typescript-eslint/no-explicit-any': 'off',
+    'unused-imports/no-unused-imports': 'warn',
 
-        // Currently annoying as we have a lot of bootstraping code, lets fix later
-        '@typescript-eslint/no-unused-vars': 'off',
-    },
+    // Currently annoying as we have a lot of bootstraping code, lets fix later
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 
-    ignorePatterns: [
-        "out",
-        "dist",
-        "**/*.d.ts"
-    ]
+  ignorePatterns: ['out', 'dist', '**/*.d.ts'],
 }
--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Open the project in VSCode and open debug view (Ctrl+Shift+D).
 To run the extension: Then select `Extension` and press `F5` or hit play.
 To run tests: Then select `Tests` and press `F5` or hit play.
 
+# Linting
+Using ESLint
+And prettier as an ESLint plugin https://github.com/prettier/eslint-plugin-prettier
+
 # Ideas
 
 See [docs](docs/) for idea documentation.
