e5740947c3444824f40185a9131728e0386a92ad
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-09-26 17:27:02 -0700

    More formatting bullshit

--- configurations/.eslintrc.js
+++ configurations/.eslintrc.js
@@ -1,39 +1,45 @@
+/** 
+ * https://eslint.org/docs/latest/use/configure/configuration-files
+ * Unfortunately js files under configurations/ will not be linted. I was unable to find a work around
+ * in a reasonable amount of time.
+*/
+
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: 'module',
-    project: './tsconfig.json', // Path to your tsconfig.json
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
 
-  plugins: ['@typescript-eslint', 'unused-imports', 'prettier'],
+  plugins: ["@typescript-eslint", "unused-imports", "prettier"],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
     // This is a plugin splitting up long comments into multiple lines
-    'plugin:comment-length/recommended',
+    "plugin:comment-length/recommended",
   ],
   rules: {
     // Add your custom rules here
-    '@typescript-eslint/no-explicit-any': 'off',
-    'unused-imports/no-unused-imports': 'warn',
+    "@typescript-eslint/no-explicit-any": "off",
+    "unused-imports/no-unused-imports": "warn",
 
     // Currently annoying as we have a lot of bootstraping code, lets fix later
-    '@typescript-eslint/no-unused-vars': 'off',
+    "@typescript-eslint/no-unused-vars": "off",
 
     // Enforce consistent braces
-    curly: ['error', 'all'],
+    curly: ["error", "all"],
 
     // This is the native rule that enforces multiline comments to be wrapped in a block
-    'multiline-comment-style': ['error', 'bare-block'],
+    "multiline-comment-style": ["error", "bare-block"],
     // This is a plugin splitting up long comments into multiple lines
-    'comment-length/limit-single-line-comments': [
-      'warn',
+    "comment-length/limit-single-line-comments": [
+      "warn",
       {
-        mode: 'overflow-only',
+        mode: "overflow-only",
         maxLength: 80,
         logicalWrap: true,
         ignoreUrls: true,
@@ -44,10 +50,10 @@ module.exports = {
 
     // https://github.com/prettier/eslint-plugin-prettier#options
     // I'm including options here just to avoid having to have a separate .prettierrc.js file
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'all',
+        trailingComma: "all",
         semi: false,
         singleQuote: true,
         plugins: [],
@@ -59,11 +65,11 @@ module.exports = {
   },
 
   ignorePatterns: [
-    'out',
-    'dist',
-    '**/*.d.ts',
-    '**/*.js',
-    '**/*.mjs',
-    'testing-sandbox',
+    "out",
+    "dist",
+    "**/*.d.ts",
+    "**/*.js",
+    "**/*.mjs",
+    "testing-sandbox",
   ],
-}
+};
