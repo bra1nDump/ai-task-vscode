24f3a39c59af33eeca63c71ee1320f4b40c9d669
c1deepinside <vanya0759@gmail.com>
2023-11-03 12:51:55 +0300

    can send select text

--- src/chrome-extension/sendMessage.ts
+++ src/chrome-extension/sendMessage.ts
@@ -1,11 +1,30 @@
 import * as WebSocket from 'ws'
+import * as vscode from 'vscode'
 import { webSocket } from './server'
 
 export function sendMessageToChrome() {
-  const num = Math.random()
+  const text = getSelectedText()
   if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-    console.log('kek')
-    webSocket.send('как думаешь что это? lulik' + String(num))
+    webSocket.send(`vscodeText:${text}`)
+    void vscode.window.showInformationMessage(
+      'The message was sent to the gpt chat',
+    )
+  } else {
+    void vscode.window.showErrorMessage(
+      'There is no working browser extension to send the message',
+    )
   }
-  console.log(num)
+}
+
+function getSelectedText() {
+  const editor = vscode.window.activeTextEditor
+  if (!editor) {
+    void vscode.window.showErrorMessage('No active editor!')
+    return
+  }
+
+  const selection = editor.selection
+  const text = editor.document.getText(selection)
+
+  return text
 }
