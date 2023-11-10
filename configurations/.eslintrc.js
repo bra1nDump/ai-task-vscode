876ccd7589d8a0b9bb255262cfe56ea0e9e4813c
Vetiver <dmitriy_serga@mail.ru>
2023-11-10 03:59:48 +0300

    start dev

--- configurations/.eslintrc.js
+++ configurations/.eslintrc.js
@@ -61,7 +61,7 @@ module.exports = {
         importOrder: [],
         importOrderSeparation: true,
         importOrderSortSpecifiers: true,
-        endOfLine: 'lf',
+        endOfLine: 'auto',
       },
     ],
   },
