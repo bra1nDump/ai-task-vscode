ae22955c8bf7a40d1aff600dfd3fd4c3c365f812
Кривицкий Дмитрий <dmitriy_serga@mail.ru>
2023-11-12 01:16:07 +0300

    add buttons "right + click" menu

--- example-projects/testing-sandbox/helloWorld.ts
+++ example-projects/testing-sandbox/helloWorld.ts
@@ -1,4 +1,3 @@
-//  @task @errors Parametrize this function with a name and remove this comment line. Don't use semi-colons in the end of lines.
-export function helloWorld() {
-  console.log(`Hello World!`);
+export function helloWorld(name: string) {
+  console.log(`Hello ${name}!`)
 }
